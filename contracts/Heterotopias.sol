b3e606efb0c3ffec704eb7b5ce09579cebf46df6
errethakbe <746655859@qq.com>
2021-08-17 16:29:01 +0800

    Finish Heterotopias storage sturctror.

--- contracts/Heterotopias.sol
+++ contracts/Heterotopias.sol
@@ -2,137 +2,35 @@
 
 pragma solidity ^0.8.0;
 
-
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-import "./common/meta-transactions/ContentMixin.sol";
-import "./common/meta-transactions/NativeMetaTransaction.sol";
-
-
-contract OwnableDelegateProxy {}
-
-contract ProxyRegistry {
-    mapping(address => OwnableDelegateProxy) public proxies;
-}
-
-/**
- * @title Heterotopias
- * Heterotopias - a contract for non-fungible Heterotopias online magazine.
- */
-contract Heterotopias is ContextMixin, ERC721URIStorage, NativeMetaTransaction, Ownable {
-    using SafeMath for uint256;
-
-    address proxyRegistryAddress;
-    uint256 private _currentTokenId = 0;
-
-    constructor(
-        address _proxyRegistryAddress
-    ) public ERC721("Heterotopias", "Heterotopias") {
-        proxyRegistryAddress = _proxyRegistryAddress;
-        _initializeEIP712("Heterotopias");
+contract Heterotopia is ERC721URIStorage {
+    using SafeERC20 for IERC20;
+    using Counters for Counters.Counter;
+    struct Issue {
+        //
+        uint256 issue_id;
+        address publisher;
+        mapping (address => uint256) baseline;
+        mapping (address => bool) special_edition;
+        uint256 total_edition_amount;
+        address special_payment_addr;
     }
-    // Replace transfer function by Buy functions 
-    /**
-     * @dev See {IERC721-transferFrom}.
-     */
-    function transferFrom(address from, address to, uint256 tokenId) public virtual override {}
-
-    /**
-     * @dev See {IERC721-safeTransferFrom}.
-     */
-    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {}
-
-    /**
-     * @dev See {IERC721-safeTransferFrom}.
-     */
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {}
-
-    /**
-     * @dev Mints a token to an address with a tokenURI.
-     * @param _to address of the future owner of the token
-     */
-
-    /**
-     * @dev buy a NFT from the owner.
-     *
-     * Requirements:
-     *
-     * - `_token_addr` list of tokens(address) can be accepted for payment.
-     *                 `A token address` can be ERC-20 token contract address or `address(0)`(ETH).
-     * - `_price_options` payment and price options. Each token can have multiple payment options
-     *                    for different subscription periods.
-     * - `_asset_URI` identifier for this asset.
-     *
-     * Emits a {CreateAsset} event.
-     * - Emitted {CreateAsset} event contains an unique id.
-     */
-    function buy(
-        uint64 _toke_id,
-        uint8 _payment_token_index
-    ) public payable{
-
+    
+    struct Edition {
+        uint256 NFT_id;
+        uint256 transfer_price;
+        address token_address;
+        uint256 issue_id;
     }
+    mapping (uint256 => Issue) issue_series;
+    mapping (uint256 => Edition) editions;
 
-    function mintTo(address _to) public onlyOwner {
-        uint256 newTokenId = _getNextTokenId();
-        _mint(_to, newTokenId);
-        _incrementTokenId();
-    }
 
-    /**
-     * @dev calculates the next token ID based on value of _currentTokenId
-     * @return uint256 for the next token ID
-     */
-    function _getNextTokenId() private view returns (uint256) {
-        return _currentTokenId.add(1);
-    }
 
-    /**
-     * @dev increments the value of _currentTokenId
-     */
-    function _incrementTokenId() private {
-        _currentTokenId++;
-    }
 
-    function baseTokenURI() virtual public pure returns (string memory);
-
-    function tokenURI(uint256 _tokenId) override public pure returns (string memory) {
-        return string(abi.encodePacked(baseTokenURI(), Strings.toString(_tokenId)));
-    }
-
-    /**
-     * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
-     */
-    function isApprovedForAll(address owner, address operator)
-        override
-        public
-        view
-        returns (bool)
-    {
-        // Whitelist OpenSea proxy contract for easy trading.
-        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
-        if (address(proxyRegistry.proxies(owner)) == operator) {
-            return true;
-        }
-
-        return super.isApprovedForAll(owner, operator);
-    }
-
-    /**
-     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
-     */
-    function _msgSender()
-        internal
-        override
-        view
-        returns (address sender)
-    {
-        return ContextMixin.msgSender();
-    }
 
 }
