0d4c47b14787de9f52e5050aef233eb5ef09f31e
errethakbe <746655859@qq.com>
2021-08-23 01:08:00 +0800

    modify IERC721 in openzeppelin to make transfer payable

--- contracts/royaltyNFT.sol
+++ contracts/royaltyNFT.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity >= 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract royaltyNFT is ERC721URIStorage , Ownable{
+import "./IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import "./IERC721.sol";
+contract royaltyNFT is Context, ERC165, IERC721, IERC721Metadata{
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
     Counters.Counter private _issueIds;
@@ -17,7 +23,7 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         // This structure is used to store the public attributes of same series of NFTs.
         uint192 issue_id;
         // Used to identify which series it is.
-        address publisher;
+        address payable publisher;
         // Publisher of this series NFTs
         uint64 total_edition_amount;
         // Number of NFTs included in this series
@@ -36,6 +42,8 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         // `A token address` can be ERC-20 token contract address or `address(0)`(ETH).
         mapping (address => uint256) first_sell_price;
         // The price should be payed when this series NTFs are minted.
+        // 这两个mapping如果存在token_addr看价格是不可以等于0的，如果等于0的话会导致判不支持
+        // 由于这个价格是写死的，可能会诱导用户的付款倾向
     }
 
     struct Edition {
@@ -60,7 +68,7 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
     event publishSuccess(
         string name,
         uint192 issue_id,
-        address publisher,
+        address payable publisher,
         uint256 total_edition_amount,
         uint8 royalty_fee,
 	    address[] token_addrs,
@@ -68,12 +76,12 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
 	    uint256[] first_sell_price
     );
 
-    event mintSuccess (
+    event buySuccess (
         address publisher,
         uint256 NFT_id,
         uint256 transfer_price,
         address transfer_token_addr,
-        address minter
+        address buyer
     );
     event transferSuccess(
         uint256 NFT_id,
@@ -90,7 +98,9 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         _issueIds.increment();
     }
 
-
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs.io/ipfs/";
+    }
     /**
      * @dev Determine NFT price before transfer.
      *
@@ -114,8 +124,8 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         uint256[] memory _first_sell_price,
         uint8 _royalty_fee,
         uint64 _total_edition_amount,
-        string _name,
-        string _ipfs_hash
+        string memory _name,
+        string memory _ipfs_hash
     ) external {
         _issueIds.increment();
         uint192 max_192 = type(uint192).max;
@@ -124,31 +134,51 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         Issue storage new_issue = issues_by_id[new_issue_id];
         new_issue.name = _name;
         new_issue.issue_id = new_issue_id;
-        new_issue.publisher = msg.sender;
+        new_issue.publisher = payable(msg.sender);
         new_issue.royalty_fee = _royalty_fee;
         new_issue.total_edition_amount = _total_edition_amount;
         new_issue.remain_edition_amount = _total_edition_amount;
         // ?此处的ipfshash是代表着pdf还是metadata
         new_issue.ipfs_hash = _ipfs_hash;
-        for (int _token_addr_id = 0; _token_addr_id < _token_addrs.length; _token_addr_id++){
-            new_issue.base_royaltyfee[_token_addr[_token_addr_id]] = _base_royaltyfee[_token_addr_id];
-            new_issue.first_sell_price[_token_addr[_token_addr_id]] = _first_sell_price[_token_addr_id];
+        for (uint8 _token_addr_id = 0; _token_addr_id < _token_addrs.length; _token_addr_id++){
+            new_issue.base_royaltyfee[_token_addrs[_token_addr_id]] = _base_royaltyfee[_token_addr_id];
+            new_issue.first_sell_price[_token_addrs[_token_addr_id]] = _first_sell_price[_token_addr_id];
         }
         emit publishSuccess();
     }
-    function mintNFT(
+
+    function buy(
         uint192 _issue_id, 
         address _token_addr
-    ) public payable returns (uint128) {
-        require(issues_by_id[_issue_id].remain_edition_amount) > 0, "royaltyNFT: There is no NFT remain in this issue.");
-        uint128 new_edition_id = uint128(issues_by_id[_issue_id].total_edition_amount - issues_by_id[_issue_id].remain_edition_amount);
-        uint128 new_NFT_id = (_issue_id << 64) | new_edition_id;
-        storage new_NFT = edition_by_id[new_edition_id];
+    ) public payable {
+        require(isIssueExist(_issue_id), "royaltyNFT: This issue is not exist.");
+        require(issues_by_id[_issue_id].first_sell_price[_token_addr] != 0, "royaltyNFT: The token your selected is not supported.");
+        if (_token_addr == address(0)) {
+            require(msg.value == issues_by_id[_issue_id].first_sell_price[_token_addr], "royaltyNFT: not enought ETH");
+            issues_by_id[_issue_id].publisher.transfer(issues_by_id[_issue_id].first_sell_price[_token_addr]);
+        }
+        else {
+            IERC20(_token_addr).safeTransferFrom(msg.sender, address(this), issues_by_id[_issue_id].first_sell_price[_token_addr]);
+        }
+        _mintNFT(_issue_id);
+
+    }
+
+
+
+    function _mintNFT(
+        uint192 _issue_id
+    ) internal returns (uint256) {
+        require((issues_by_id[_issue_id].remain_edition_amount > 0), "royaltyNFT: There is no NFT remain in this issue.");
+        uint128 new_editions_id = uint128(issues_by_id[_issue_id].total_edition_amount - issues_by_id[_issue_id].remain_edition_amount);
+        uint256 new_NFT_id = (_issue_id << 64) | new_editions_id;
+        Edition storage new_NFT = editions_by_id[new_editions_id];
         new_NFT.NFT_id = new_NFT_id;
-        new_NFT.price = 0;
-        new_NFT.token_addr = 0;
+        new_NFT.transfer_price = 0;
+        new_NFT.token_addr = address(0);
         new_NFT.is_on_sale = false;
         issues_by_id[_issue_id].remain_edition_amount -= 1;
+        _setTokenURI(new_NFT_id, issues_by_id[_issue_id].ipfs_hash);
         _safeMint(msg.sender, new_NFT_id);
         return new_NFT_id;
     }
@@ -167,7 +197,7 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
      * - `_token_addr` address of the token this transcation used, address(0) represent ETH.
      * - `_price` The amount of `_token_addr` should be payed for `_NFT_id`
      */
-    function determine_price(
+    function determinePrice(
         uint256 _NFT_id, 
         address _token_addr,
         uint256 _price
@@ -178,6 +208,9 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         emit determinePriceSuccess(_NFT_id, _token_addr, _price);
     }
 
+    function determinePriceAndApprove() public {
+        
+    }
     
 
     function _beforeTokenTransfer (
@@ -186,7 +219,7 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         uint256 NFT_id
     ) internal override {
         
-        if (!_is_exclude_from_fee[from] && !_is_exclude_from_fee[to]) {
+        if (to != issues_by_id[getIssueIdByNFTId(NFT_id)].publisher && from != issues_by_id[getIssueIdByNFTId(NFT_id)]) {
             require(editions_by_id[NFT_id].price != 0, "royaltyNFT: price should be set");
             // 抽手续费
         } 
@@ -211,32 +244,32 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
     function safeTransferFrom(
         address from,
         address to,
-        uint256 NFT_id;
+        uint256 NFT_id,
         bytes memory _data
     ) public payable override {
         
         super.safeTransferFrom(from, to, NFT_id, _data);
         _afterTokenTransfer(NFT_id);
     }
-    function isIssueExist(uint192 _issue_id) public view return (bool) {
+    function isIssueExist(uint192 _issue_id) public view returns (bool) {
         if (issues_by_id[_issue_id].issue_id == 0)
             return false;
         return true;
     }
-    function isEditionExist(uint256 _NFT_id) public view return (bool) {
-        if (edition_by_id[_NFT_id].NFT_id == 0)
+    function isEditionExist(uint256 _NFT_id) public view returns (bool) {
+        if (editions_by_id[_NFT_id].NFT_id == 0)
             return false;
         return true;
     }
 
-    function getIssueIdByNFTId(uint256 _NFT_id) public view return(uint192) {
+    function getIssueIdByNFTId(uint256 _NFT_id) public view returns (uint192) {
         return uint192(_NFT_id >> 64);
     }
 
-    function getNFTIdByIssueId(uint192 _issue_id) public view return(uint256 [] memory) {
+    function getNFTIdByIssueId(uint192 _issue_id) public view returns (uint256 [] memory) {
         uint256 [] memory NFT_ids = new uint256 [](issues_by_id[_issue_id].total_edition_amount);
-        for (int edition_id = 0; edition_id < issues_by_id[_issue_id].total_edition_amount; edition_id++){
-            NFT_ids[edition_id] = uint256(_issue_id << 64 | edition_id);
+        for (int editions_id = 0; editions_id < issues_by_id[_issue_id].total_edition_amount; editions_id++){
+            NFT_ids[editions_id] = uint256(_issue_id << 64 | editions_id);
         }
         return NFT_ids;
     }
@@ -245,13 +278,6 @@ contract royaltyNFT is ERC721URIStorage , Ownable{
         return _amount.mul(_royalty_fee).div(
             10**2
         );
-    }
-    function excludeFromRoyaltyFee(address account) public onlyOwner {
-        _is_exclude_from_fee[account] = true;
-    }
-    
-    function includeInRoyaltyFee(address account) public onlyOwner {
-        _is_exclude_from_fee[account] = false;
     }
     
     function setRoyaltyPercent(uint64 _issue_id, uint8 _royalty_fee) external onlyOwner {
